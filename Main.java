debd7a134a646866d6b4c515faae20f92422d5ea
notyulya <yuliya-malceva@mail.ru>
2024-03-10 10:17:47 +0300

    new method added

--- Main.java
+++ Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("123");
     }
+
+    public static void example() {
+        int a = 0;
+        int b = 5;
+        int c = a+b;
+    }
 }
