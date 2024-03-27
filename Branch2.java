276bdb5e2b3d4614415c224c42bdaddd381c7d83
notyulya <yuliya-malceva@mail.ru>
2024-03-28 01:23:00 +0300

    создана коллекция из 5 элемнтов, элементы 2 и 4 выводятся в консоль

--- Branch2.java
+++ Branch2.java
@@ -1,6 +1,23 @@
-public class Branch2 {
-    public static void branch2 (){
+import java.util.ArrayList;
+import java.util.List;
 
-        System.out.println("wow branch2!");
+public class Branch2 {
+    public static void branch2(String[] args) {
+        elements2and4();
+    }
+
+    public static int elements2and4() {
+        //создать коллекцию, добавить 5 элементов и вывести 2 и 4
+        List<Integer> list = new ArrayList<>();
+        list.add(44);
+        list.add(38);
+        list.add(910);
+        list.add(1);
+        list.add(74);
+        int element2 = list.get(2);
+        int element4 = list.get(4);
+        System.out.println(element2);
+        System.out.println(element4);
+        return 0;
     }
 }
